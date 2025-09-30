849aa8a154ffb99624ce00464610d3d25e712550
sem1colon <vamsikaza@outlook.in>
2025-10-01 01:21:02 +0530

    Cleaned Up the repo

--- ui/cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:8100',
-    supportFile: 'cypress/support/e2e.ts',
-    specPattern: 'cypress/e2e/**/*.cy.ts',
-  },
-});
